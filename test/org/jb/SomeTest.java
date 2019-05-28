6a2e034c9887750688ec089dc0ff1f54548109de
Anton Arhipov <ant.arhipov@gmail.com>
2019-05-28 10:41:04 +0300

    vcs.xml

---
+++ test/org/jb/SomeTest.java
@@ -0,0 +1,24 @@
+package org.jb;
+
+import org.junit.Test;
+
+public class SomeTest {
+
+
+  @Test
+  public void test1() {
+
+  }
+
+  @Test
+  public void test2() {
+
+  }
+
+  @Test
+  public void test3() {
+
+  }
+
+
+}
